cf5651d659b2fc71485a6e00524274d0a8fac867
Scott Langeberg <slangeberg@gmail.com>
2014-07-05 17:03:41 -0500

    wrote very basic tabular data display

--- src/main/groovy/com/greekadonis/gandas/DataFrame.groovy
+++ src/main/groovy/com/greekadonis/gandas/DataFrame.groovy
@@ -1,5 +1,43 @@
 package com.greekadonis.gandas
 
-class DataFrame extends HashMap<String, Object> {
+class DataFrame extends LinkedHashMap<String, Object> {
 
+  Set<String> getColumns() {
+    keySet()
+  }
+
+  List<Object> getColumnValues(String column) {
+    this[column]
+  }
+
+  /**
+   * Print contents in tabular format, as seen in pandas
+   */
+  @Override
+  String toString() {
+    String msg = System.lineSeparator()
+    //print columns
+    getColumns().each {
+      msg += "$it\t"
+    }
+    msg += System.lineSeparator()
+
+    //just print row by row, for now
+    Set<String> columns = getColumns()
+    List<Object> firstColumn = getColumnValues(columns[0])
+    int numRows = firstColumn.size()
+    int numColumns = columns.size()
+
+    (0..numRows-1).each { int row ->
+      (0..numColumns-1).each { int col ->
+        msg += "${getColumnValues(columns[col])[row]}\t"
+      }
+      msg += System.lineSeparator()
+    }
+    msg += "\t"
+  }
+
+  private String printRow() {
+
+  }
 }
--- src/test/scripts/dataframe.groovy
+++ src/test/scripts/dataframe.groovy
@@ -4,10 +4,11 @@ def df = new DataFrame(
     name: ["Braund", "Cummings"],
     age: [22, 38],
     fare: [7.25, 71.38],
-    survided: [false, true]
+    survived: [false, true]
 )
 
 println "df: $df"
+println "df.toString(): ${df.toString()}"
 
 println "df.age: ${df.age}"
 
