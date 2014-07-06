97fc7061eeb29a2741741a5ce1b80606e5a30fee
Scott Langeberg <slangeberg@gmail.com>
2014-07-05 21:35:10 -0500

    Updated test to apply(mean) on two columns of 1000000 elements, and times look good, to start with

--- src/test/groovy/com/greekadonis/gandas/DataFrameTest.groovy
+++ src/test/groovy/com/greekadonis/gandas/DataFrameTest.groovy
@@ -10,30 +10,38 @@ import spock.lang.Specification
  * Created by scott on 7/5/14.
  */
 class DataFrameTest extends Specification {
-  def "Can load lots of data"(){
 
-    StopWatch timer = new StopWatch()
-    timer.start()
+  def "Can load lots of data"(){
 
     int numEls = 1000000
     def colOneValues = []
     def colTwoValues = []
     (1..numEls).each {
       colOneValues << it
-      colTwoValues << it * 2
+      colTwoValues << (it * 2)
     }
     def params = [
         one: colOneValues,
         two: colTwoValues
     ]
 
+    ////////
+
+    StopWatch timer = new StopWatch()
+    timer.start()
+
     DataFrame df = new DataFrame(params)
     def result = df.apply(mean)
 
     println "\nCompleted w/#els: $numEls, in ${timer.time}ms"
 
+    ////////
+
+    def expectedOne = colOneValues.sum()/colOneValues.size()
+    def expectedTwo = colTwoValues.sum()/colTwoValues.size()
+
     expect:
-      result.one == colOneValues.sum()/colOneValues.size()
-      result.two == colOneValues.sum()/colOneValues.size()
+      result.one == expectedOne
+      result.two == expectedTwo
   }
 }
